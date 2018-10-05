4ec4db8808bbd61caadd209b44577be472521c8f
vanayun <yunvana@naver.com>
2018-10-05 22:35:05 +0900

    eslint setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const eslint = require('gulp-eslint')
 
 gulp.task('default', function() {
+  // ESLint 실행
+  gulp
+    .src(['es6/**/*.js', 'public/es6/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+  // 노드 소스
   gulp
     .src('es6/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+  // 브라우저 소스
   gulp
     .src('public/es6/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('public/dist'));
-});
+    .pipe(gulp.dest('public/dist'))
+})
